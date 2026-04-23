5e90a9e20d10b7148ab8b23cbba94eb146e2f3d4
lokmankhodziri <lokmankhodziri@gmail.com>
2026-04-24 00:58:58 +0800

    Update new resume

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -9,7 +9,7 @@ import githubLight from "../../assets/github-light.svg";
 import githubDark from "../../assets/github-dark.svg";
 import linkedinLight from "../../assets/linkedin-light.svg";
 import linkedinDark from "../../assets/linkedin-dark.svg";
-import CV from "../../assets/Resume_Update_V5.pdf";
+import CV from "../../assets/Resume_Update_V7";
 import { useTheme } from "../common/useTheme";
 
 const Hero: React.FC = () => {
